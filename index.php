774ab36165e7046e13a457bf5700d4102a2a4899
rycahayash <rycahaya.205025@mhs.its.ac.id>
2022-12-16 19:17:15 +0700

    signup and login front end

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
               <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                 <div class="row">
                   <div class="col-lg-12">
-                    <h2>Get The Latest App From App Stores</h2>
-                    <p>Chain App Dev is an app landing page HTML5 template based on Bootstrap v5.1.3 CSS layout provided by TemplateMo, a great website to download free CSS templates.</p>
+                    <h2>Seleksi Pegawai</h2>
+                    <p>Kementrian Kelautan dan Perikanan Provinsi Jawa Timur</p>
                   </div>
                   <div class="col-lg-12">
                     <div class="white-button first-button scroll-to-section">
